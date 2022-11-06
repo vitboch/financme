5f3427e359d9eb257ed0a2306d190a4f9f0ba5ec
Vitaly Bochkarev <vitalybochkarev@Book.local>
2022-11-06 18:09:06 +0700

    start project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>App</h1>
+      <img src={`${process.env.PUBLIC_URL}/financme.png`} alt="logo" />
+    </>
+  );
+};
+
+export default App;
